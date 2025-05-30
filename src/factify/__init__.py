70278eafbc3b0dbf2e641f69b533556ddaeded11
ulxsth <ulxsth@gmail.com>
2025-05-30 21:22:38 +0900

    chore: install ruff

--- src/factify/__init__.py
+++ src/factify/__init__.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def read_root():
     return {"message": "Hello from FastAPI with Docker & Poetry!"}
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
