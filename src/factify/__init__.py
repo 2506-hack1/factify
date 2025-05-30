d4f73cebaeae95174219b16c6af3f6445878be83
ulxsth <ulxsth@gmail.com>
2025-05-30 22:06:08 +0900

    chore: test

--- src/factify/__init__.py
+++ src/factify/__init__.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -14,6 +15,4 @@ async def read_item(item_id: int, q: str = None):
 
 
 if __name__ == "__main__":
-    import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
