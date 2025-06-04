c71e292829a79cf58549753b94120b7747e5b593
ulxsth <ulxsth@gmail.com>
2025-06-04 13:52:10 +0900

    fix: こみっとみす

---
+++ infra/infra/s3_cloudfront_stack.py
@@ -0,0 +1,116 @@
+from aws_cdk import (
+    Stack,
+    aws_s3 as s3,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins,
+    aws_s3_deployment as s3deploy,
+    aws_iam as iam,
+    CfnOutput,
+    RemovalPolicy,
+)
+from constructs import Construct
+
+
+class S3CloudFrontStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, api_endpoint: str = None, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # S3 bucket for hosting static website
+        website_bucket = s3.Bucket(
+            self,
+            "WebsiteBucket",
+            bucket_name=f"factify-webapp-{self.account}-{self.region}",
+            website_index_document="index.html",
+            website_error_document="index.html",  # SPAのため404もindex.htmlに
+            public_read_access=False,  # CloudFront経由でのみアクセス
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=RemovalPolicy.DESTROY,  # 開発環境用
+            auto_delete_objects=True,  # 開発環境用
+        )
+
+        # CloudFront Origin Access Control (OAC)
+        oac = cloudfront.CfnOriginAccessControl(
+            self,
+            "WebsiteOAC",
+            origin_access_control_config=cloudfront.CfnOriginAccessControl.OriginAccessControlConfigProperty(
+                name="WebsiteOAC",
+                origin_access_control_origin_type="s3",
+                signing_behavior="always",
+                signing_protocol="sigv4",
+            )
+        )
+
+        # CloudFront Distribution
+        distribution = cloudfront.Distribution(
+            self,
+            "WebsiteDistribution",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3Origin(
+                    website_bucket,
+                    origin_access_control_id=oac.attr_id,
+                ),
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                cache_policy=cloudfront.CachePolicy.CACHING_OPTIMIZED,
+            ),
+            default_root_object="index.html",
+            error_responses=[
+                # SPAのため、すべての404を index.html にリダイレクト
+                cloudfront.ErrorResponse(
+                    http_status=404,
+                    response_http_status=200,
+                    response_page_path="/index.html",
+                    ttl=None,
+                ),
+                cloudfront.ErrorResponse(
+                    http_status=403,
+                    response_http_status=200,
+                    response_page_path="/index.html",
+                    ttl=None,
+                ),
+            ],
+        )
+
+        # S3 bucket policy for CloudFront OAC
+        bucket_policy = iam.PolicyStatement(
+            actions=["s3:GetObject"],
+            resources=[f"{website_bucket.bucket_arn}/*"],
+            principals=[iam.ServicePrincipal("cloudfront.amazonaws.com")],
+            conditions={
+                "StringEquals": {
+                    "AWS:SourceArn": f"arn:aws:cloudfront::{self.account}:distribution/{distribution.distribution_id}"
+                }
+            }
+        )
+        website_bucket.add_to_resource_policy(bucket_policy)
+
+        # Environment variables for build
+        environment_variables = {}
+        if api_endpoint:
+            environment_variables["VITE_API_ENDPOINT"] = api_endpoint
+
+        # Outputs
+        CfnOutput(
+            self,
+            "WebsiteBucketName",
+            value=website_bucket.bucket_name,
+            description="S3 Bucket Name"
+        )
+
+        CfnOutput(
+            self,
+            "WebsiteURL",
+            value=f"https://{distribution.distribution_domain_name}",
+            description="Website URL"
+        )
+
+        CfnOutput(
+            self,
+            "CloudFrontDistributionId",
+            value=distribution.distribution_id,
+            description="CloudFront Distribution ID"
+        )
+
+        # Store for other stacks
+        self.website_bucket = website_bucket
+        self.distribution = distribution
+        self.website_url = f"https://{distribution.distribution_domain_name}"
